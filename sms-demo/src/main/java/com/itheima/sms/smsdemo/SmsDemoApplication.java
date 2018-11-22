f5ae3af99c29568ebc3c61054e660bb9377e77eb
huchaoyin <huchaoyin@qq.com>
2018-11-22 15:38:42 +0800

    第12天

---
+++ sms-demo/src/main/java/com/itheima/sms/smsdemo/SmsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.itheima.sms.smsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsDemoApplication.class, args);
+    }
+}
